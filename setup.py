bdc04d0970ef0b2ec340f881ba4c555bf3715669
Neel Chauhan <neel@neelc.org>
2016-06-12 17:37:20 -0400

    Add setup.py & setup.cfg

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+
+setup(name="OnionLauncher",
+	version="0.0.1",
+	description="Launcher for Tor",
+	license = "BSD",
+	author="Neel Chauhan",
+	author_email="neel@neelc.org",
+	url="https://www.github.com/neelchauhan/OnionLauncher/",
+	packages=["OnionLauncher"],
+	entry_points={'gui_scripts': ['OnionLauncher=OnionLauncher.main:main_loop']},
+	package_data={"OnionLauncher": ["data/*"]},
+	install_requires=[
+		"stem",
+	],
+	#data_files=[
+	#	(sys.prefix + "/share/pixmaps", ["icons/scalable/onionlauncher.svg"]),
+	#	(sys.prefix + "/share/applications", ["data/onionlauncher.desktop"]),
+	#],
+	classifiers=[
+		"Environment :: X11 Applications :: Qt",
+		"Intended Audience :: End Users/Desktop",
+		"License :: OSI Approved :: BSD License",
+		"Operating System :: POSIX",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 3",
+	],
+)
