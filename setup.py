d3d093178fd1c73b8f7ed0b669bbd98a94916a11
Neel Chauhan <neel@neelc.org>
2016-06-24 12:23:45 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import sys
 
 setup(name="OnionLauncher",
-	version="0.0.1",
+	version="0.0.2",
 	description="Launcher for Tor",
 	license = "BSD",
 	author="Neel Chauhan",
